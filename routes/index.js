bf6c9a246f2238afe40a83e0e76b4cc1bc3309b2
divinemphame <mphametadiwanashe@gmail.com>
2025-11-07 18:05:05 +0200

    checkin project

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 //this is to Define my routes here
-router.get('/', (req, res) => {res.send('Hello World!');});
+//router.get('/', (req, res) => {res.send('Hello World!');});
+
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 // server.js
 const express = require('express');
+
+const mongodb = require('./data/database'); // Example of including a database module
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } 
+  else {
+    app.listen(PORT, () => {
+      console.log(`Database is listening and node Running on port ${PORT}`)});
+}
+});
 
