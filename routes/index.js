155ab4fb447ca2f960691c22e861d68e91c78adf
divinemphame <mphametadiwanashe@gmail.com>
2025-11-09 02:08:08 +0200

    installed swagger

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+    //#swagger.tags= ['Hello World']
+    res.send('Hello World!');
+}); 
+
 //this is to Define my routes here
 //router.get('/', (req, res) => {res.send('Hello World!');});
 
